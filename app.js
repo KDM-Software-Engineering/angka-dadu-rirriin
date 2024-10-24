52415e6853c14e05b0dfaa54f7ee4284e1138601
rirriin <meilian237650@student.smkn1kandeman.sch.id>
2024-10-24 14:53:40 +0700

    selesai

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 function main() {
-  
-
-
-}
+        let hasilDadu = [1, 5, 2, 6, 2, 3];
+        let ganjil = 0;
+        let genap = 0;
+      
+        for (let i = 0; i<hasilDadu.length; i++) {
+        
+        if (hasilDadu[i] % 2 == 1 ) {
+          ganjil++;
+        }
+        else if (hasilDadu[i] % 2 == 0) {
+          genap++;
+        }
+    }
+      console.log (`Raka  mendapatkan angka ganjil sebanyak ${genap} kali dan angka genap sebanyak ${ganjil} kali`)
+    }
+    main()
 
 module.exports = main
